5f516e12547e6daa4413bdf708ded8775b6fb8b2
Bruno Chiconato <brunochiconato01@gmail.com>
2024-10-17 19:50:16 -0300

    Update
    - Classe 'DataExtractor' ajustada para buscar os arquivos dentro do diretório e lê-los de fato.

--- app/data_extractor.py
+++ app/data_extractor.py
@@ -1,5 +1,6 @@
 import pandas as pd # type: ignore
 import os
+import glob
 
 class DataExtractor:
     """
@@ -11,14 +12,14 @@ class DataExtractor:
 
     def validate_input_path(self, input_path: str) -> str:
         """
-        Valida se o caminho do arquivo de entrada existe.
+        Valida se o caminho do diretório de entrada existe.
 
         Levanta uma exceção FileNotFoundError se o caminho especificado não existir.
 
         Parameters:
         -----------
         input_path : str
-            Caminho do arquivo de entrada a ser validado.
+            Caminho do diretório a ser validado.
 
         Returns:
         --------
@@ -28,7 +29,7 @@ class DataExtractor:
         Raises:
         -------
         FileNotFoundError
-            Se o caminho do arquivo de entrada não existir.
+            Se o caminho do diretório não existir.
         """
         if not os.path.exists(input_path):
             raise FileNotFoundError(f"O caminho '{input_path}' não existe.")
@@ -36,14 +37,14 @@ class DataExtractor:
     
     def read_csv_data(self, input_path: str) -> pd.DataFrame:
         """
-        Lê dados de um arquivo CSV e retorna um DataFrame.
+        Lê dados de um único arquivo CSV em um diretório e retorna um DataFrame.
 
-        Verifica se o caminho de entrada é válido antes de ler o arquivo.
+        Verifica se o caminho de entrada é válido e busca pelo único arquivo CSV no diretório.
 
         Parameters:
         -----------
         input_path : str
-            Caminho do arquivo CSV a ser lido.
+            Caminho do diretório onde o arquivo CSV está localizado.
 
         Returns:
         --------
@@ -53,21 +54,26 @@ class DataExtractor:
         Raises:
         -------
         FileNotFoundError
-            Se o caminho do arquivo de entrada não existir.
+            Se o caminho do diretório ou nenhum arquivo CSV for encontrado.
         """
         ok_input_path = self.validate_input_path(input_path)
-        return pd.read_csv(ok_input_path)
+        # Busca o único arquivo CSV no diretório
+        csv_files = glob.glob(os.path.join(ok_input_path, '*.csv'))
+        if len(csv_files) != 1:
+            raise FileNotFoundError(f"Esperado exatamente um arquivo CSV no diretório '{input_path}', mas encontrou {len(csv_files)}.")
+        
+        return pd.read_csv(csv_files[0])
 
     def read_json_data(self, input_path: str) -> pd.DataFrame:
         """
-        Lê dados de um arquivo JSON e retorna um DataFrame.
+        Lê dados de um único arquivo JSON em um diretório e retorna um DataFrame.
 
-        Verifica se o caminho de entrada é válido antes de ler o arquivo.
+        Verifica se o caminho de entrada é válido e busca pelo único arquivo JSON no diretório.
 
         Parameters:
         -----------
         input_path : str
-            Caminho do arquivo JSON a ser lido.
+            Caminho do diretório onde o arquivo JSON está localizado.
 
         Returns:
         --------
@@ -77,21 +83,25 @@ class DataExtractor:
         Raises:
         -------
         FileNotFoundError
-            Se o caminho do arquivo de entrada não existir.
+            Se o caminho do diretório ou nenhum arquivo JSON for encontrado.
         """
         ok_input_path = self.validate_input_path(input_path)
-        return pd.read_json(ok_input_path)
+        json_files = glob.glob(os.path.join(ok_input_path, '*.json'))
+        if len(json_files) != 1:
+            raise FileNotFoundError(f"Esperado exatamente um arquivo JSON no diretório '{input_path}', mas encontrou {len(json_files)}.")
+        
+        return pd.read_json(json_files[0])
     
     def read_parquet_data(self, input_path: str) -> pd.DataFrame:
         """
-        Lê dados de um arquivo Parquet e retorna um DataFrame.
+        Lê dados de um único arquivo Parquet em um diretório e retorna um DataFrame.
 
-        Verifica se o caminho de entrada é válido antes de ler o arquivo.
+        Verifica se o caminho de entrada é válido e busca pelo único arquivo Parquet no diretório.
 
         Parameters:
         -----------
         input_path : str
-            Caminho do arquivo Parquet a ser lido.
+            Caminho do diretório onde o arquivo Parquet está localizado.
 
         Returns:
         --------
@@ -101,7 +111,11 @@ class DataExtractor:
         Raises:
         -------
         FileNotFoundError
-            Se o caminho do arquivo de entrada não existir.
+            Se o caminho do diretório ou nenhum arquivo Parquet for encontrado.
         """
         ok_input_path = self.validate_input_path(input_path)
-        return pd.read_parquet(ok_input_path)
+        parquet_files = glob.glob(os.path.join(ok_input_path, '*.parquet'))
+        if len(parquet_files) != 1:
+            raise FileNotFoundError(f"Esperado exatamente um arquivo Parquet no diretório '{input_path}', mas encontrou {len(parquet_files)}.")
+        
+        return pd.read_parquet(parquet_files[0])
