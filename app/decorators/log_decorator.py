319a2f21904f68358e63d5557c45527bd562fb7b
Bruno Chiconato <brunochiconato01@gmail.com>
2024-10-19 19:08:14 -0300

    Update
    Adicionada uma docstring para o decorador log_decorator.

--- app/decorators/log_decorator.py
+++ app/decorators/log_decorator.py
@@ -6,6 +6,30 @@ logger.remove()
 logger.add('../app.log', colorize=True, format="{time:DD/MM/YYYY HH:mm:ss} {message} {level}", level="INFO")
 
 def log_decorator(func):
+    """
+    Decorador para adicionar logs de execução a uma função.
+
+    Este decorador registra automaticamente mensagens de log antes e depois
+    da execução de uma função, além de capturar e registrar quaisquer 
+    exceções que possam ocorrer durante a execução da função. As mensagens
+    de log são gravadas em um arquivo especificado no formato configurado.
+
+    Parameters:
+    -----------
+    func : callable
+        A função que será decorada.
+
+    Returns:
+    --------
+    callable
+        A função decorada com o comportamento de logging.
+
+    Raises:
+    -------
+    Exception
+        Relança qualquer exceção que seja capturada durante a execução da função,
+        permitindo que ela seja tratada em um nível superior.
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         logger.info(f'Executando a função {func.__name__}.')
